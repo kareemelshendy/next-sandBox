807fc0eb41cf176b45f761569922d3970f159500
Karem Elshendy <karemalshendy@gmail.com>
2022-07-18 21:56:56 +0200

    modify checkboxs

--- src/hoc/character.hoc.tsx
+++ src/hoc/character.hoc.tsx
@@ -3,26 +3,79 @@ import { Characters, CheckBox } from 'components';
 
 import { useGetCharacters } from 'hooks';
 import { useForm } from 'react-hook-form';
+import { Fragment, useEffect } from 'react';
 interface Props {}
 
 export const CharactersHOC: React.FC<Props> = () => {
 	// const { data, loading, error } = useGetCharacters();
-	const { control, handleSubmit, register } = useForm();
+	const { control, handleSubmit, register, watch, setValue } = useForm();
+
+	const all = watch('all');
+	const fruits = watch('fruits');
+
+	useEffect(() => {
+		console.log('all', all);
+		if (all === '*') {
+			setValue('fruits', ['0', '1', '2', '3', '4', '5']);
+		} else {
+			setValue('fruits', []);
+		}
+	}, [all]);
+
+	// useEffect(() => {
+	// 	console.log('fruits', fruits);
+	// 	if (fruits.length === 6) {
+	// 		// setValue('all', '*');
+	// 	} else {
+	// 		setValue('all', false);
+	// 	}
+	// }, [fruits]);
 
 	const onSubmit = (data: any) => {
 		console.log(data);
+		console.log(data.fruits.every((item: any) => Boolean(item) === true));
 	};
 	return (
 		<div className='container'>
 			<form onSubmit={handleSubmit(onSubmit)}>
-				{Array.from({ length: 6 }).map((_, index) => (
+				{/* {Array.from({ length: 6 }).map((_, index) => (
 					<CheckBox
 						key={index}
 						name='fruits'
 						control={control}
 						id={`${index}`}
 					/>
+				))} */}
+
+				{Array.from({ length: 6 }).map((_, index) => (
+					<Fragment key={index}>
+						<input
+							{...register('fruits')}
+							type='checkbox'
+							id={`${index}`}
+							name='fruits'
+							value={`${index}`}
+						/>
+						<label htmlFor={`${index}`}>{index}</label>
+					</Fragment>
 				))}
+
+				<div>
+					<input
+						type='checkbox'
+						{...register('all')}
+						name={'all'}
+						id={'*'}
+						checked={
+							(fruits?.length === 6 &&
+								fruits.every(
+									(item: any) => Boolean(item) === true
+								)) ||
+							false
+						}
+					/>
+					<label htmlFor='*'>all</label>
+				</div>
 
 				{/* <input
 					{...register('kareem')}
